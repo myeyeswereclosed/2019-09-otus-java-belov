f3298e712ab90f136e7571de567f3b5d1ad32492
myeyeswereclosed <belovi-tech@mail.ru>
2019-09-28 07:47:14 -0400

    HW01-MAVEN initial

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,9 @@
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+    public static void main(String[] args) {
+        System.out.println(
+            Strings.repeat("London is the capital of Great Britain\n", 3)
+        );
+    }
+}
